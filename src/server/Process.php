719c76703dabae6b22cd659f6d94c28401f4f51e
as2252258@163.com <as22522582>
2019-07-18 18:19:05 +0800

    modify macos can't run

--- src/server/Process.php
+++ src/server/Process.php
@@ -34,7 +34,7 @@ class Process
 
 		static::watch(APP_PATH);
 		$isAdd = swoole_event_add(static::$inotify, [Process::class, 'check']);
-		var_dump($isAdd);
+		var_dump($isAdd, static::$inotify);
 		if (!$isAdd) {
 			echo 'add process error. ' . swoole_last_error();
 		}
